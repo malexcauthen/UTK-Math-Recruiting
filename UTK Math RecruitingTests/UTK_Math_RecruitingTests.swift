28cb825fd65caf262e3ffa698cc58efab85b75c7
malexcauthen <mcauthen@vols.utk.edu>
2015-12-08 09:48:55 -0500

    Initial Commit

---
+++ UTK Math RecruitingTests/UTK_Math_RecruitingTests.swift
@@ -0,0 +1,36 @@
+//
+//  UTK_Math_RecruitingTests.swift
+//  UTK Math RecruitingTests
+//
+//  Created by Alex Cauthen on 12/8/15.
+//  Copyright © 2015 malexcauthen. All rights reserved.
+//
+
+import XCTest
+@testable import UTK_Math_Recruiting
+
+class UTK_Math_RecruitingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
